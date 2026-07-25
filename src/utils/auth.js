24001e415a493621aeaaaaf4e8d465a99f110749
Hisham <hisham@mernan.sa>
2026-07-25 18:17:39 +0300

    Fallback to ken_role in auth.js for dashboards

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,14 +2,14 @@ import { jwtDecode } from "jwt-decode";
 
 export const getUserRole = () => {
   const token = localStorage.getItem("ken_token");
-  if (!token) return null;
+  if (!token) return localStorage.getItem("ken_role") || null;
 
   try {
     const decoded = jwtDecode(token);
 
-    return decoded.role || null;
+    return decoded.role || localStorage.getItem("ken_role") || null;
   } catch (error) {
     console.error("Invalid token:", error);
-    return null;
+    return localStorage.getItem("ken_role") || null;
   }
 };
